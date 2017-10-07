d6bca55a9f3cb6a1bd8970c7f11d1de5faeb39c9
Matt Burman <matt@mattburman.com>
2017-10-07 16:25:12 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const { resolve } = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const postcssMiddleware = require('postcss-middleware');
+
+const debug = require('debug')('hs:app');
+
+const app = express();
+
+// view engine setup
+app.set('views', resolve('views'));
+app.set('view engine', 'pug');
+
+// middleware setup and use
+// uncomment after placing your favicon in /src
+// app.use(favicon(resolve('src', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(resolve('src')));
+
+// load and set up routes
+const { router: index } = require('./routes/index');
+
+app.use('/', index);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+	const err = new Error('Not Found');
+	err.status = 404;
+	next(err);
+});
+
+// error handler
+app.use((err, req, res, next) => {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = app.get('env') === 'development' ? err : {};
+
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
+});
+
+module.exports = { app };
---
+++ server.js
@@ -0,0 +1,51 @@
+const { app } = require('./app');
+const debug = require('debug')('hs:server');
+const { createServer } = require('http');
+
+/**
+ * Get port from environment and store in Express.
+ */
+const { PORT = 3000 } = process.env;
+app.set('port', PORT);
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+const onError = err => {
+	if (err.syscall !== 'listen') throw err;
+
+	// handle specific listen errors with friendly messages
+	const handleErrors = {
+		EACCES: () => {
+			console.error(`Port ${PORT} requires elevated privileges`);
+			process.exit(1);
+		},
+
+		EADDRINUSE: () => {
+			console.error(`Port ${PORT} is already in use`);
+			process.exit(1);
+		},
+
+		default: err => {
+			throw err;
+		}
+	};
+
+	handleErrors[err.code in handleErrors ? err.code : 'default'](err);
+};
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+const onListening = () => {
+	debug(`Listening on port ${PORT}`);
+};
+
+/**
+ * Create HTTP server.
+ */
+const server = createServer(app);
+
+server.listen(PORT);
+server.on('error', onError);
+server.on('listening', onListening);
