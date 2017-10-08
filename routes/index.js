8f3a5ed09bed4271b4e75c36606bff296c35de1f
Matt Burman <matt@mattburman.com>
2017-10-07 21:37:33 -0700

    Team blacklist

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,19 @@ const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 if (!GITHUB_TOKEN) throw new Error("Supply GitHub token");
 
 const graphql = `{
-  organization(login:"hacksheffield") {
+ organization(login:"hacksheffield") {		
     avatarUrl
     databaseId
-    repository(name:"people"){ name object(expression:"master:_data/people.yml"){
+    repository(name:"people"){
+      name
+      object(expression:"master:_data/people.yml"){
+      	repository{
+          object(expression:"master:_config/team-blacklist.json"){
+            ... on Blob {
+              text
+            }
+          }
+        }
         ... on Blob {
           text
         }
@@ -56,11 +65,12 @@ router.get('/', (req, res, next) => {
       YAML.parse(graphqlRes.data.data.organization.repository.object.text).forEach(person => {
         people[person.github] = person;
       });
+      const teamBlacklist = JSON.parse(graphqlRes.data.data.organization.repository.object.repository.object.text);
 
       console.log(people);
       teams.forEach(team => team.members = team.members.edges.map(edge => edge.node));
       // const people = graphqlRes.data.data.organization.teams.edges.map(edge => edge.node)[0].members.edges.map(edge => edge.node);
-      res.render('index', { title: 'Teams', teams: teams, people });
+      res.render('index', { title: 'Teams', teams: teams, people, teamBlacklist });
     })
     .catch((err) => {
       console.log(err);
