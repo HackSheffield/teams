001032569638fe5f35ce4c76f4ef0835daae2804
Matt Burman <matt@mattburman.com>
2017-10-07 18:38:59 -0700

    Dynamism

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,64 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+if (!GITHUB_TOKEN) throw new Error("Supply GitHub token");
+
+const graphql = `{
+  organization(login:"hacksheffield") {
+    avatarUrl
+    databaseId
+    repository(name:"people"){
+      name
+      object(expression:"master:_data/people.yml"){
+        ... on Blob {
+          text
+        }
+      }
+    }
+    teams (first: 100){
+      edges {
+        node{
+          description
+          teamsUrl
+          url
+          slug
+          combinedSlug
+          name
+          members (first: 100){
+            edges {
+              node {
+                name
+                bio
+                url
+                login
+                location
+                bioHTML
+                email
+                avatarUrl
+                websiteUrl
+                isCampusExpert
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}`;
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-	res.render('index', { title: 'Teams' });
+  axios.post('https://api.github.com/graphql', { query: graphql }, { headers: {'Authorization': `Bearer ${GITHUB_TOKEN}`}})
+    .then((graphqlRes) => {
+      console.log(graphqlRes.data);
+      const people = graphqlRes.data.data.organization.teams.edges.map(edge => edge.node)[0].members.edges.map(edge => edge.node);
+      res.render('index', { title: 'Teams', people: people, teamname: 'HackSheffield TEAM'});
+    })
+    .catch((err) => {
+      console.log(err);
+      res.render('error', {});
+  });
 });
 
 module.exports = { router };
